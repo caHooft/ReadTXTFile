374fa3193fb52c98831e506923e0a0c50b0d7be3
Hooft, C.A. <hoof1200@student.nhl.nl>
2021-07-09 13:47:37 +0200

    Basic TXT reader. I added a real whole and line by line reader. Line by line is not limited by memery where reading the entire pointcloud is limited by memory

--- ConsoleApplicationReadFile/Program.cs
+++ ConsoleApplicationReadFile/Program.cs
@@ -10,6 +10,54 @@ namespace ConsoleApplicationReadFile
     {
         static void Main(string[] args)
         {
+            // The files used in this example are created in the topic
+            // How to: Write to a Text File. You can change the path and
+            // file name to substitute text files of your own.
+
+            // Example #1
+            // Read the file as one string.
+            string text = System.IO.File.ReadAllText(@"C:\TestFolder\WriteText.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            // Example #2
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"C:\TestFolder\WriteText2.txt");
+
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+            // Example #1
+            // Read the file as one string.
+            //string testText = System.IO.File.ReadAllText(@"C:\TestFolder\TestRead.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", testText);
+
+            // Example #2
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] testLines = System.IO.File.ReadAllLines(@"C:\TestFolder\TestRead2.txt");
+
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in testLines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
     }
 }
